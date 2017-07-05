0343db060d914449a870884e31f80609fdd57cbb
Juan Sebastian Espinosa <jnset@hotmail.com>
2017-07-05 07:33:10 -0500

    EquipeSelect

--- Assets/Scripts/equipeSelect.cs
+++ Assets/Scripts/equipeSelect.cs
@@ -49,6 +49,41 @@ public class equipeSelect : MonoBehaviour {
         }
         ToConsole("Finaliza la búsqueda");
     }
+    
+    public void MostrarTodosEquipos(int valor)
+    {
+        valor.ToString();
+        var ds = new DataService("dataBaseScout.db");
+        ToConsole("Empieza la busqueda " + valor);
+        var equipe = ds.GetAddressEquipe();
+        ToConsole("Bucando TODOS");
+        equipe = ds.GetEquipeName();
+        ToConsole("AllTeams: ");
+        ToConsole(equipe);
+        foreach (var x in equipe)
+        {
+         var eqNome = x.nome.ToString();
+         EquipeS.text = eqNome.ToString();
+         //equipito.options.Add(new Dropdown.OptionData() { text = eqNome });
+        }
+        equipito.RefreshShownValue();
+        equipito.value = 1;
+        equipito.value = 0;
+        equipito.RefreshShownValue();
+        ToConsole("Finaliza la búsqueda");
+    }
+
+    //ejemplo para manejar el dropdown
+    public void DropdownInput(int input)
+    {
+        Debug.Log("Seleccionada: " + input);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void ToConsole(IEnumerable<equipeTabela> x)
     {
@@ -58,64 +93,10 @@ public class equipeSelect : MonoBehaviour {
             ToConsole(equipo.ToString().ToUpper());
         }
     }
-   
+
     private void ToConsole(string msg)
     {
         DebugText.text += System.Environment.NewLine + msg;
         Debug.Log(msg);
-    }
-    
-    //ejemplo para manejar el dropdown
-    public void DropdownInput(int input) {
-        Debug.Log("Seleccionada: " + input);
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void MostrarTodosEquipos(int valor)
-    {
-        valor.ToString();
-        var ds = new DataService("dataBaseScout.db");
-        ToConsole("Empieza la busqueda " + valor);
-        var equipe = ds.GetAddressEquipe();
-
-        switch (valor)
-        {
-            case 1:
-                ToConsole("Bucando TODOS");
-                equipe = ds.GetEquipeName();
-                ToConsole("AllTeams: ");
-                ToConsole(equipe);
-                foreach (var x in equipe)
-                {
-                    var eqNome = x.nome.ToString();
-                    EquipeS.text = eqNome.ToString();
-                    //equipito.options.Add(new Dropdown.OptionData() { text = eqNome });
-                            
-                }
-                equipito.RefreshShownValue();
-                break;
-            case 2:
-                ToConsole("Bucando ALL");
-                equipe = ds.GetEquipeName();
-                ToConsole("ALL Equipos: ");
-                ToConsole(equipe);
-                foreach (var x in equipe)
-                {
-                    var eqNome = x.nome.ToString();
-                    EquipeS.text = eqNome.ToString();
-                    //equipito.options.Add(new Dropdown.OptionData() { text = eqNome});
-                }
-                equipito.RefreshShownValue();
-                break;
-        }
-        equipito.value = 1;
-        equipito.value = 0;
-        equipito.RefreshShownValue();
-        ToConsole("Finaliza la búsqueda");
     }
 }
